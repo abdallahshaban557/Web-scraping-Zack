70ccf6b292eac54562f871e1001a412f34df9dd8
abdullahshaaban <abdullah.shaaban91@gmail.com>
2017-11-23 22:59:39 -0800

    Fixed bugs and issues.

--- login_scraper_example.py
+++ login_scraper_example.py
@@ -1,8 +1,6 @@
 #CTRL+ALT+N to Run the python script
-#from urllib.request import urlopen as uReq
-#import urllib.parse
 from bs4 import BeautifulSoup
-
+from datetime import datetime, timedelta
 import requests
 import time
 from selenium import webdriver
@@ -31,7 +29,8 @@ try:
     
 
     #Load original Excel file - and select the needed sheet - get maximum row number
-    Result_File = load_workbook('results_file/messedupb.xlsx')
+    #Result_File = load_workbook('results_file/messedupb.xlsx')
+    Result_File = load_workbook('results_file/redo_eps.xlsx')
     Result_sheet = Result_File.worksheets[0]
     Max_Row_Count = Result_sheet.max_row
 
@@ -46,12 +45,30 @@ try:
         Result_File.save('results_file/FINAL.xlsx')  
 
         Ticker_Symbol = Result_sheet.cell(row=i, column=6).value
-        EPS_Date = str(Result_sheet.cell(row=i, column=11).value)
+        
+        #Get date from the initial file. Minus and add a day for better results
+        New_EPS_date_format = Result_sheet.cell(row=i, column=3).value.date()
+        EPS_Day = New_EPS_date_format.strftime('%#d')
+        EPS_Month = New_EPS_date_format.strftime('%#m')
+        EPS_Year = New_EPS_date_format.strftime('%Y')
+        EPS_Date = EPS_Month + "/" + EPS_Day + "/" + EPS_Year
+        
+        EPS_Date_plus_day = New_EPS_date_format + timedelta(days=1)
+        EPS_Day = EPS_Date_plus_day.strftime('%#d')
+        EPS_Month = EPS_Date_plus_day.strftime('%#m')
+        EPS_Year = EPS_Date_plus_day.strftime('%Y')
+        EPS_Date_plus_day = EPS_Month + "/" + EPS_Day + "/" + EPS_Year
+
+        EPS_Date_minus_day = New_EPS_date_format - timedelta(days=1)
+        EPS_Day = EPS_Date_minus_day.strftime('%#d')
+        EPS_Month = EPS_Date_minus_day.strftime('%#m')
+        EPS_Year = EPS_Date_minus_day.strftime('%Y')
+        EPS_Date_minus_day = EPS_Month + "/" + EPS_Day + "/" + EPS_Year
 
         #Get all EPS values in data sheet
-        IBES_Estimize_actual = str(Result_sheet.cell(row=i, column=2).value)
-        IBES_actual = str(Result_sheet.cell(row=i, column=4).value)
-        IBES_adj_actual = str(Result_sheet.cell(row=i, column=5).value)
+        IBES_Estimize_actual = Result_sheet.cell(row=i, column=2).value
+        IBES_actual = Result_sheet.cell(row=i, column=4).value
+        IBES_adj_actual = Result_sheet.cell(row=i, column=5).value
 
         #Locate the search box on top of the page
         Ticker_Symbol_Search = driver.find_element_by_name('search-q')
@@ -76,35 +93,48 @@ try:
             time.sleep(1)
 
             #Row number in the website EPS table
-            EPS_Table_Row_Number = 2
+            EPS_Table_Row_Number = 1
         
             #Go through the whole table of EPS
             while True:
                 try:
+                    #Grab date from the website
                     Date_Of_EPS = driver.find_element_by_xpath('//*[@id="earnings_announcements_earnings_table_wrapper"]/div[3]/div[3]/div[2]/div/table/tbody/tr['+ str(EPS_Table_Row_Number) +']/td').text
-                
-                    if Date_Of_EPS == EPS_Date:
+
+                    #compare date from the data sheet with the website, and assign the right values based on EPS
+                    if Date_Of_EPS == EPS_Date or Date_Of_EPS == EPS_Date_minus_day or Date_Of_EPS == EPS_Date_plus_day:
                         Reported_EPS = driver.find_element_by_xpath('//*[@id="earnings_announcements_earnings_table"]/tbody/tr[' + str(EPS_Table_Row_Number) + ']/td[4]').text
-                        Reported_EPS = Reported_EPS[1:]
-                        #Condition to add the value of IBES_RIGHT in data sheet
-                        if IBES_actual == Reported_EPS or IBES_adj_actual == Reported_EPS:
-                            Result_sheet.cell(row=i, column=9).value = "1"
-                        elif IBES_Estimize_actual != Reported_EPS:
-                            Result_sheet.cell(row=i, column=9).value = "0"
+                        if Reported_EPS != "--":
+                            Reported_EPS = Reported_EPS.replace("$","")
+                            Reported_EPS = float(Reported_EPS)
+                            Result_sheet.cell(row=i, column=11).value = Reported_EPS
                         else:
-                            Result_sheet.cell(row=i, column=9).value = "0"
+                            Result_sheet.cell(row=i, column=11).value = Reported_EPS
+
+                        #Condition to add the value of IBES_RIGHT in data sheet
+                        if Reported_EPS == "--":
+                            Result_sheet.cell(row=i, column=9).value = 'N/A'
+                        elif IBES_actual == Reported_EPS or IBES_adj_actual == Reported_EPS:
+                            Result_sheet.cell(row=i, column=9).value = 1
+                        elif IBES_Estimize_actual != Reported_EPS:
+                            Result_sheet.cell(row=i, column=9).value = 0
+                        else:
+                            Result_sheet.cell(row=i, column=9).value = 0
                          
                         #Condition to add the value of ESTIMIZE to data sheet
-                        if IBES_Estimize_actual == Reported_EPS:
-                            Result_sheet.cell(row=i, column=10).value = "1"
+                        if Reported_EPS == "--":
+                            Result_sheet.cell(row=i, column=10).value = 'N/A'
+                        elif IBES_Estimize_actual == Reported_EPS:
+                            Result_sheet.cell(row=i, column=10).value = 1
                         else:
-                            Result_sheet.cell(row=i, column=10).value = "0"
+                            Result_sheet.cell(row=i, column=10).value = 0
 
                     #Increment counter for loop
                     EPS_Table_Row_Number += 1
                    
                 
                 except Exception as e:
+                    print(str(e))
                     break         
         except Exception as e:
             Result_sheet.cell(row=i, column=9).value = 'N/A'
